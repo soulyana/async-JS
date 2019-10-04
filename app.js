21c5cdc976789f917ec97f3cdf5d17b1e3c2c569
Soulyana <soulyana.lakew@accenturefederal.com>
2019-10-04 18:34:30 -0400

    From Promises to Async/Await

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ getRecipe();
  * Determines what happens after the event has happened
  * implements the concept of a future value that we're expecting
  */
+/*
 
 const getIDs = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -80,3 +81,44 @@ getIDs.then(IDs => {
 }).catch(error => {
     console.log('Error!!');
 });
+*/
+
+/**
+ * From Promises to Async/Await
+ */
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([523, 883, 432, 974]);
+    }, 1500);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID);
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {title: 'Italian Pizza', publisher: 'Jonas'};
+            resolve(`${pub}: ${recipe.title}`);
+        }, 1500, publisher);
+    })
+};
+
+async function getRecipesAW() {
+    const IDs = await getIDs;
+    console.log(IDs);
+    const recipe = await getRecipe(IDs[2]);
+    console.log(recipe);
+    const related = await getRelated('Souly');
+    console.log(related);
+
+    return recipe;
+}
+getRecipesAW().then(result => console.log(`${result} is the best ever!`));
